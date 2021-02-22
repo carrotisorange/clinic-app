fdc5cac320927b003ff54e1d8b5f7ccc1e8a9108
= <=>
2021-02-22 16:57:48 +0800

    finishes the layout for the clinic app.

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,72 @@
+<!--
+=========================================================
+* Paper Dashboard 2 - v2.0.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-dashboard-2
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+   @yield('title')
+  </title>
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+  <!-- CSS Files -->
+  <link href="{{ asset('/paper-dashboard-master/assets/css/bootstrap.min.css') }}" rel="stylesheet" />
+  <link href="{{ asset('/paper-dashboard-master/assets/css/paper-dashboard.css?v=2.0.1') }}" rel="stylesheet" />
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href="{{ asset('/paper-dashboard-master/assets/demo/demo.css') }}" rel="stylesheet" />
+  <script src="https://kit.fontawesome.com/c93b9e4bc6.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="">
+  <div class="wrapper ">
+@include('layouts.sidebar')
+    <div class="main-panel">
+      <!-- Navbar -->
+  @include('layouts.navbar')
+      <!-- End Navbar -->
+    @yield('content')
+     @include('layouts.footer')
+     @include('layouts.logout')
+    </div>
+  </div>
+  <!--   Core JS Files   -->
+  <script src="{{ asset('/paper-dashboard-master/assets/js/core/jquery.min.js') }}"></script>
+  <script src="{{ asset('/paper-dashboard-master/assets/js/core/popper.min.js') }}"></script>
+  <script src="{{ asset('/paper-dashboard-master/assets/js/core/bootstrap.min.js') }}"></script>
+  <script src="{{ asset('/paper-dashboard-master/assets/js/plugins/perfect-scrollbar.jquery.min.js') }}"></script>
+  <!--  Google Maps Plugin    -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+  <!-- Chart JS -->
+  <script src="{{ asset('/paper-dashboard-master/assets/js/plugins/chartjs.min.js') }}"></script>
+  <!--  Notifications Plugin    -->
+  <script src="{{ asset('/paper-dashboard-master/assets/js/plugins/bootstrap-notify.js') }}"></script>
+  <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="{{ asset('/paper-dashboard-master/assets/js/paper-dashboard.min.js?v=2.0.1') }}" type="text/javascript"></script><!-- Paper Dashboard DEMO methods, don't include it in your project! -->
+  <script src="{{ asset('/paper-dashboard-master/assets/demo/demo.js') }}"></script>
+  <script>
+    $(document).ready(function() {
+      // Javascript method's body can be found in assets/assets-for-demo/js/demo.js
+      demo.initChartsPages();
+    });
+  </script>
+</body>
+
+</html>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,51 @@
+<div class="sidebar" data-color="white" data-active-color="danger">
+    <div class="logo">
+      {{--<a href="/dashboard" class="simple-text logo-mini">
+        <div class="logo-image-small">
+           <img src="{{ asset('/paper-dashboard-master/assets/img/logo-small.png') }}"> 
+        </div>
+         <p>CT</p> 
+      </a>
+      --}}
+      <a href="/dashboard" class="simple-text logo-normal">
+        Clinic App
+         {{-- <div class="logo-image-big">
+         <img src="{{ asset('/paper-dashboard-master/assets/img/logo-big.png') }}"> 
+        </div>  --}}
+      </a>
+    </div>
+    <div class="sidebar-wrapper">
+      <ul class="nav">
+        <li class="active ">
+          <a href="/dashboard">
+            <i class="fas fa-tachometer-alt"></i>
+            <p>Dashboard</p>
+          </a>
+        </li>
+        <li>
+          <a href="./icons.html">
+            <i class="fas fa-user-injured"></i>
+            <p>Patient Medical Record</p>
+          </a>
+        </li>
+        <li>
+          <a href="./map.html">
+            <i class="fas fa-prescription"></i>
+            <p>Medicine Inventory</p>
+          </a>
+        </li>
+        <li>
+          <a href="./notifications.html">
+            <i class="far fa-calendar-check"></i>
+            <p>Patient Appointments</p>
+          </a>
+        </li>
+        <li>
+          <a href="./user.html">
+            <i class="nc-icon nc-single-02"></i>
+            <p>Manage Accounts</p>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
